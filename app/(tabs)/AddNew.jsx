5f9308d09a01941eba28b3e1a34b52c935c2fdd5
Naveen145-ai <naveensrinivas145@gmail.com>
2025-05-27 22:06:25 +0530

    native1

---
+++ app/(tabs)/AddNew.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function AddNew() {
+  return (
+    <View>
+      <Text>Add New Item</Text>
+    </View>
+  );
+}
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,61 +1,53 @@
-import { View, Text,Image } from 'react-native'
-import React from 'react'
-import { getLocalStorage } from '../service/Storage'
+import { View, Text, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { getLocalStorage } from '../service/Storage';
 import Feather from '@expo/vector-icons/Feather';
-//import Colors from '../../constant/Colors'
+// import Colors from '../../constant/Colors'
 
 export default function Header() {
+  const [user, setUser] = useState(null);
 
-    const [user,setUser] = useState();
+  useEffect(() => {
+    GetUserDetail();
+  }, []);
 
-    useEffect(()=>{
-        GetUserDetail();
-    },[])
+  const GetUserDetail = async () => {
+    const userInfo = await getLocalStorage('userDetail');
+    console.log(userInfo);
+    setUser(userInfo);
+  };
 
-    const GetUserDetail=async()=>{
-        const userInfo=await getLocalStorage('userDetail');
-        console.log(userInfo);
-        setUser(userInfo);
-        
-    }
   return (
-    <View style={{
-      marginTop:20
-    }}>
+    <View style={{ marginTop: 20 }}>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '100%',
+        }}
+      >
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: 10,
+          }}
+        >
+          <Image
+            source={require('./../assets/images/medical.png')}
+            style={{ width: 45, height: 45 }}
+          />
 
-      <View style={{
-        display:'flex',
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
-          width:'100%'
-      }}>
+          <Text style={{ fontSize: 25, fontWeight: 'bold' }}>
+            Hello {user?.displayName ?? 'Guest'} 👋
+          </Text>
+        </View>
 
-      
-        <View style={{
-            display:'flex',
-            flexDirection:'row',
-            alignItems:'center',
-            gap:10,
-
-            
-
-        }}>
-      <Image source={require('./../assets/images/medical.png')}
-      style={{
-        width:45,
-        height:45
-      }}
-      />
-
-      <Text stylele={{
-        fontSize:25,
-        fontWeight:'bold'
-      }}>Hello {user.displayName} 👋</Text>
-      </View>
-      <Feather name="settings" size={34} color={Colors.DARK_GRAY} />
-
+        <Feather name="settings" size={34} color="gray" />
       </View>
     </View>
-  )
+  );
 }
